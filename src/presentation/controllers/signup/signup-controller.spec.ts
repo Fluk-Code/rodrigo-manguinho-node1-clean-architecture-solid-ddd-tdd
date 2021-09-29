481a5790c60adc69f74ad8aefc0b0364ff350f9a
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-09-28 23:11:47 -0300

    refactor: move all signup protocols to a single file

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -1,8 +1,6 @@
 import { SignUpController } from './signup-controller'
-import { EmailValidator } from '../protocols'
-import { ServerError, MissingParamError, InvalidParamError } from '../errors'
-import { AddAccount, AddAccountModel } from '../../domain/usecases/add-account'
-import { AccountModel } from '../../domain/models/account'
+import { ServerError, MissingParamError, InvalidParamError } from '../../errors'
+import { EmailValidator, AccountModel, AddAccount, AddAccountModel } from './signup-protocols'
 
 interface SutTypes {
   sut: SignUpController
